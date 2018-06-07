4e67a1a09a0ab9cc13e29f97ef76a58f963dda56
Jack Hadjicosti <jack.hadjicosti@gmail.com>
2018-06-07 15:06:24 +0200

    Added Wwise streaming assets (excluding large files)

---
+++ StreamingAssets/Audio/GeneratedSoundBanks2/Wwise_IDs.h
@@ -0,0 +1,100 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID BULLET_IMPACT = 3921814510U;
+        static const AkUniqueID CHARACTER_DIE = 2990357609U;
+        static const AkUniqueID CHARACTER_VO = 810970808U;
+        static const AkUniqueID CHARACTER_VO_TAUNT = 3333935137U;
+        static const AkUniqueID CHICKIE_ATTACK = 850200368U;
+        static const AkUniqueID CHICKIE_CLUCK = 170707952U;
+        static const AkUniqueID CHICKIE_CLUCK_STOP = 3045187071U;
+        static const AkUniqueID CHICKIE_DIE = 1925025166U;
+        static const AkUniqueID CHICKIE_SPAWN = 3863523225U;
+        static const AkUniqueID GRENADE_IMPACT = 2833450406U;
+        static const AkUniqueID MENU_SELECT = 4203375351U;
+        static const AkUniqueID PLAY_MUSIC = 2932040671U;
+        static const AkUniqueID POWERSPEEDUP = 484764186U;
+        static const AkUniqueID WALKING = 340271938U;
+        static const AkUniqueID WEAPON_FIRE = 3702403660U;
+        static const AkUniqueID WEAPON_PICKUP = 3674859828U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace CHARACTER
+        {
+            static const AkUniqueID GROUP = 436743010U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CKICKIE = 1419073108U;
+                static const AkUniqueID COOKIEMONSTER = 2448265983U;
+                static const AkUniqueID KFCCOLONEL = 3024550177U;
+                static const AkUniqueID KING = 1997285300U;
+                static const AkUniqueID MCCLOWN = 707051848U;
+            } // namespace SWITCH
+        } // namespace CHARACTER
+
+        namespace WEAPON
+        {
+            static const AkUniqueID GROUP = 3893417221U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID FLAMETHROWER = 2691847895U;
+                static const AkUniqueID GRENADE = 3255686181U;
+                static const AkUniqueID MINIGUN = 3776419406U;
+                static const AkUniqueID PISTOL = 324443136U;
+            } // namespace SWITCH
+        } // namespace WEAPON
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID CHICKIE = 157587169U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID POWERUPS = 2600048462U;
+        static const AkUniqueID PROTAGONIST = 288643575U;
+        static const AkUniqueID UI = 1551306167U;
+        static const AkUniqueID WEAPONS = 1467963052U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
